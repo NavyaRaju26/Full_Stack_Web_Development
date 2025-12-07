5a16d93e003624c653a724143b2fd37e6035b30c
NavyaRaju26 <nr0642387@gmail.com>
2025-12-07 23:23:04 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,32 @@
+import EventRegistration from "./FSWD/Sample/EventRegistration";
+export default function App() {
+  return <EventRegistration />;
+}
+
+
+import Welcome from "./FSWD/Sample/Welcome";
+export default function App()
+{
+  return(<Welcome/>);
+}
+
+
+import PropsExample from "./FSWD/Sample/PropsExample";
+export default function App()
+{
+  return<PropsExample title="Full Stack Web Development" name="Welcome to Web Development"/>;
+}
+
+
+import StateExample from "./FSWD/Sample/StateExample";
+export default function App()
+{
+  return< StateExample/>;
+}
+
+
+import StudentForm from "./FSWD/Sample/Studentform";
+export default function App()
+{
+  return< StudentForm/>;
+}
